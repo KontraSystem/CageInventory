f26a754134f2dc515ab5e51b47c3fff4b8a3cce9
Mateo Matijevic Bosnjak <mxm9894@rit.edu>
2022-02-01 23:19:33 +0100

    Added several new feature and a small content update.

--- web/src/pages/Inventory.js
+++ web/src/pages/Inventory.js
@@ -1,12 +1,21 @@
 import React from "react";
 import NavigationBar from "../components/NavigationBar";
+import SearchBar from "../components/SearchBar";
+import Card from '../components/InventoryCard';
 
 export default function Inventory() {
     return (
         <div>
             <NavigationBar/>
-            <h2>Home page</h2>
-            <p>https://prnt.sc/26myk9g</p>
+            <h2 className="center-text">Inventory</h2>
+            <div className="container">
+                <SearchBar/>
+                <div>
+                    <Card/>
+                    <Card/>
+                </div>
+
+            </div>
         </div>
     );
 }
