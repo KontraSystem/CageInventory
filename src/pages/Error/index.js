d0d1b3ea99baf8086faf7bd1369cfef49a356212
Josip Muzic <josipmuzic99@gmail.com>
2022-02-28 11:13:05 +0100

    Update Error page

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,30 +1,32 @@
-import React from 'react'
+import NotFound from '../../assets/unDraw/404.svg'
 
 function ErrorPage() {
-	const onRetry = () => {}
-
 	return (
-		<div className="misc-wrapper">
-			<div className="misc-inner p-2 p-sm-3">
-				<div className="w-100 text-center">
-					<h2 className="mb-1">Something went wrong</h2>
-					<p className="mb-2">
-						It appears that something went wrong. Please try again later.
-					</p>
-					<div>
-						<button
-							className="bg-blue-600 hover:bg-blue-900 text-white font-bold p-2 rounded w-32 mb-3"
-							type="submit"
-							onClick={onRetry}
+		<div className="flex items-center justify-center w-screen h-screen">
+			<div className="px-4 lg:py-12">
+				<div className="lg:gap-4 lg:flex">
+					<div className="flex flex-col items-center justify-center md:py-24 lg:py-32">
+						<p className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
+							<span className="text-red-500">Oops!</span> Page not
+							found
+						</p>
+						<p className="mb-8 text-center text-gray-500 md:text-lg">
+							{"The page you're looking for doesn't exist."}
+						</p>
+						<a
+							href="/"
+							className="px-6 py-2 text-sm font-semibold text-blue-800 bg-blue-100"
 						>
-							<span>Retry</span>
-						</button>
+							Go home
+						</a>
 					</div>
-					<img
-						className="img-fluid"
-						src={`/assets/unDraw/error.svg`}
-						alt="Error image"
-					/>
+					<div className="mt-4">
+						<img
+							src={NotFound}
+							alt="Page not found image"
+							className="object-cover w-96"
+						/>
+					</div>
 				</div>
 			</div>
 		</div>
