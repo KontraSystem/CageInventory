bf714369373a6c7a27f94f8df3bea8ad74772fce
Josip Muzic <josipmuzic99@gmail.com>
2022-03-10 16:35:58 +0100

    Add ShoppingCart

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import axios from 'axios'
 
 //* Helper functions
@@ -6,12 +7,18 @@ function getToken() {
 	return { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } }
 }
 
-//* POST
-export const postLogin = (body) => axios.post(`${BASE_URL}auth/login`, body)
-export const postRegister = (body) => axios.post(`${BASE_URL}auth/register`, body)
-export const postAddToCart = (body) => axios.post(`${BASE_URL}cart/add`, body)
-
 //* GET
 export const getItems = () => axios.get(`${BASE_URL}items/all`, getToken())
 export const getItemById = (id) => axios.get(`${BASE_URL}items/${id}`, getToken())
+export const getCart = () => axios.get(`${BASE_URL}cart/view`, getToken())
 
+//* POST
+export const postLogin = (body) => axios.post(`${BASE_URL}auth/login`, body)
+export const postRegister = (body) => axios.post(`${BASE_URL}auth/register`, body)
+export const postCartAdd = (body) => axios.post(`${BASE_URL}cart/add`, body)
+export const postCartSetCount = (body) => axios.post(`${BASE_URL}cart/set-count`, body)
+export const postCartSubmit = () => axios.post(`${BASE_URL}cart/submit`, {}, getToken())
+
+//* DELETE
+export const deleteCartAll = () => axios.delete(`${BASE_URL}cart/all`, getToken())
+export const deleteCartItem = (id) => axios.delete(`${BASE_URL}cart/${id}`, getToken())
--- src/components/ShoppingCartIcon/index.js
+++ src/components/ShoppingCartIcon/index.js
@@ -1,104 +1,161 @@
-import React, { useState, useRef } from 'react';
-import { Popover } from 'react-tiny-popover';
-export default function CartIcon(props) {
-	const [shoppingOpen, isShoppingOpen] = useState(false)
+import React, { useState, useRef, useEffect, useContext } from 'react'
+import { Popover } from 'react-tiny-popover'
+import { LoadingIndicator } from '..'
+import { deleteCartAll, deleteCartItem, getCart, postCartSubmit } from '../../api'
+import DialogContext from '../../context/DialogContext'
+
+export default function CartIcon() {
+	const { setDialog } = useContext(DialogContext)
+	const [isOpen, setIsOpen] = useState(false)
 	const popoverRef = useRef(null)
-	const onShoppingClick = () => isShoppingOpen(!shoppingOpen)
-  const shopping = [
-	    { items: "Laptop", count: "1"},
-	    { items: "Cable", count: "5"},
-	    { items: "Sample", count: "5"},
-	    { items: "Neki kurac", count: "5"},
-	    { items: "Neki kurac", count: "5"}
-    ]
+	const [data, setData] = useState()
+
+	useEffect(() => {
+		if (!isOpen) return
+
+		getCart().then(({ data }) => setData(data.response))
+	}, [isOpen])
+
+	const onShoppingClick = () => setIsOpen(!isOpen)
+
+	const onDeleteItem = (id) => {
+		setDialog({
+			message: 'Do you want to remove the selected item from your cart?',
+			title: 'Are you sure?',
+			onConfirm: () =>
+				deleteCartItem(id).then(() =>
+					setData(data.filter((item) => item.id != id))
+				),
+			isRedButton: true,
+		})
+	}
+
+	const onDeleteCart = () => {
+		setDialog({
+			message: 'Do you want to remove all items from your cart?',
+			title: 'Are you sure?',
+			onConfirm: () => deleteCartAll().then(() => setData([])),
+			isRedButton: true,
+		})
+	}
+
+	const submitRequest = () => {
+		setDialog({
+			message:
+				'Do you want to submit a official request for the items in your shopping cart?',
+			title: 'Are you sure?',
+			onConfirm: () => postCartSubmit().then(() => setData([])),
+		})
+	}
+
 	return (
 		<Popover
-			isOpen={shoppingOpen}
-			positions={["bottom"]}
+			isOpen={isOpen}
+			positions={['bottom']}
 			align="end"
 			onClickOutside={onShoppingClick}
 			ref={popoverRef.current}
 			reposition={false}
-			content={(
-				<div
-					className="bg-white 
-						border-orange
-						border-4
-						rounded-lg 
-						text-center 
-						shadow-lg 
-						align-center"
-				>
-	        <table className="table-auto">
-	        	<thead className="bg-white-100 dark:bg-white-700">
-	            <tr>
-	              <th
-	                scope="col"
-	                className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
-	              >
-	                Item/Kit
-	              </th>
-								<th
-	                scope="col"
-	                className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
-	              >
-	                Count
-	              </th>
-	              <th
-	                scope="col"
-	                className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"	
-	              >
-	              	Remove
-	              </th>
-	            </tr>
-	          </thead>
-	          <tbody>
-	          	{shopping.map((item, index) => (
-	              <tr key={index} className="border-b odd:bg-white even:bg-gray-50 odd:dark:bg-white-400 even:dark:bg-gray-200 dark:border-gray-600">
-	                <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-	                  {item.items}
-	                </td>
-	                <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-	                	<span className="mr-1"><i className="fa-solid fa-minus"/></span>
-	                  {item.count}
-	                  <span className="ml-1"><i className="fa-solid fa-plus"/></span>
-	                </td>
-	                <td
-	                	scope="col"
-	                	className="py-3 px-6 text-xs  font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"	
-	              	>
-	              		<span className="place-content-center"><i className="text-red-500 fa-solid fa-trash"/></span>
-	              	</td>
-	              </tr>
-	          	))}
-	          </tbody>
-	        </table>
-		          <div className="min-h-max min-w-max ml-auto absolute right-0">
-			        	<button className=" mt-2 mb-2 mr-2 ml-2 float-left
-			        		bg-red-600 
-			        		hover:bg-red-400 
-			        		transition 
-			        		text-white 
-			        		font-bold 
-			        		p-2 
-			        		rounded 
-			        		w-15"
-			      		>Remove All</button>
-			        	<button className=" mt-2 mb-2 mr-2 ml-auto float-right
-			        		bg-green-500 
-			        		hover:bg-green-300 
-			        		transition 
-			        		text-white 
-			        		font-bold 
-			        		p-2 
-			        		rounded 
-			        		w-15"
-			      		>Go to Checkout</button>
-		          </div>
-	    	</div>
-	    )}
+			content={
+				<div className="bg-white rounded-lg text-center shadow-[0_10px_60px_0px_rgba(0,0,0,0.3)] overflow-hidden align-center">
+					{!data ? (
+						<div className="p-16">
+							<LoadingIndicator isLoading={true} />
+						</div>
+					) : data.length == 0 ? (
+						<div className="p-16">
+							<h1>Nothing has been added to the shopping cart yet</h1>
+						</div>
+					) : (
+						<>
+							<table className="table-auto">
+								<thead className="bg-orange text-white uppercase font-medium text-xs">
+									<tr>
+										<th className="py-3 px-6">Item/Kit</th>
+										<th className="py-3 px-6">Count</th>
+										<th></th>
+									</tr>
+								</thead>
+								<tbody>
+									{!data ? (
+										<div className="p-16">
+											<LoadingIndicator isLoading={true} />
+										</div>
+									) : (
+										data.map((item, index) => (
+											<tr
+												key={index}
+												className="border-b odd:bg-white even:bg-[#f768021a] text-sm text-gray-700"
+											>
+												<td className="p-2 px-4">
+													{item.item_name}
+												</td>
+												<td className="p-2">
+													<div className="flex justify-between align-center">
+														<button
+															className="rounded-full border-2 border-red-500 hover:bg-red-100  transition text-red-500 w-6 h-6"
+															onClick={() =>
+																addRequest(item.id)
+															}
+														>
+															<i className="fa-solid fa-minus" />
+														</button>
+														<span className="pt-[2px]">
+															{item.item_count}
+														</span>
+														<button
+															className="rounded-full border-2 border-green-500 hover:bg-green-100 transition text-green-500 w-6 h-6"
+															onClick={() =>
+																addRequest(item.id)
+															}
+														>
+															<i className="fa-solid fa-plus" />
+														</button>
+													</div>
+												</td>
+												<td className="p-2 px-4">
+													<div className="flex justify-center align-center">
+														<button
+															className="flex rounded-full bg-red-500 hover:bg-red-300 transition text-white p-1"
+															onClick={() =>
+																onDeleteItem(item.id)
+															}
+														>
+															<i className="fa-solid fa-trash" />
+														</button>
+													</div>
+												</td>
+											</tr>
+										))
+									)}
+								</tbody>
+							</table>
+							<div className="flex px-4 py-2 justify-between">
+								<button
+									className="bg-red-600 hover:bg-red-400 transition text-white p-2 rounded"
+									onClick={onDeleteCart}
+								>
+									Remove All
+								</button>
+								<button
+									className="bg-green-500 hover:bg-green-300 transition text-white p-2 rounded"
+									onClick={submitRequest}
+								>
+									Submit request
+								</button>
+							</div>
+						</>
+					)}
+				</div>
+			}
 		>
-			<span ref={popoverRef} className="hover:cursor-pointer" onClick={onShoppingClick}><i className="ml-10 text-lg opacity-70 fa fa-solid fa-cart-shopping text-white"/></span>
+			<span
+				ref={popoverRef}
+				className="hover:cursor-pointer"
+				onClick={onShoppingClick}
+			>
+				<i className="ml-10 text-lg opacity-70 fa fa-solid fa-cart-shopping text-white" />
+			</span>
 		</Popover>
-	);
+	)
 }
