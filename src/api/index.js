9498332b1b1d862f704cfa9a6dc34b6a77e28831
Josip Muzic <josipmuzic99@gmail.com>
2022-03-11 14:08:39 +0100

    Make helper functions in API file to avoid repetitive header

--- src/api/index.js
+++ src/api/index.js
@@ -3,22 +3,24 @@ import axios from 'axios'
 
 //* Helper functions
 export const BASE_URL = process.env.REACT_APP_API_URL
-function getToken() {
-	return { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } }
-}
+const getToken = () => { return { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } } }
+const GET = (url, header = getToken()) => axios.get(url, header)
+const DELETE = (url, header = getToken()) => axios.delete(url, header)
+const POST = (url, body = {}, header = getToken()) => axios.post(url, body, header)
+const PATCH = (url, body = {}, header = getToken()) => axios.patch(url, body, header)
 
 //* GET
-export const getItems = () => axios.get(`${BASE_URL}items/all`, getToken())
-export const getItemById = (id) => axios.get(`${BASE_URL}items/${id}`, getToken())
-export const getCart = () => axios.get(`${BASE_URL}cart/view`, getToken())
+export const getItems = () => GET(`${BASE_URL}items/all`)
+export const getItemById = (id) => GET(`${BASE_URL}items/${id}`)
+export const getCart = () => GET(`${BASE_URL}cart/view`)
 
 //* POST
-export const postLogin = (body) => axios.post(`${BASE_URL}auth/login`, body)
-export const postRegister = (body) => axios.post(`${BASE_URL}auth/register`, body)
-export const postCartAdd = (body) => axios.post(`${BASE_URL}cart/add`, body)
-export const postCartSetCount = (body) => axios.post(`${BASE_URL}cart/set-count`, body)
-export const postCartSubmit = () => axios.post(`${BASE_URL}cart/submit`, {}, getToken())
+export const postLogin = (body) => POST(`${BASE_URL}auth/login`, body, {})
+export const postRegister = (body) => POST(`${BASE_URL}auth/register`, body, {})
+export const postCartAdd = (body) => POST(`${BASE_URL}cart/add`, body)
+export const postCartSetCount = (body) => PATCH(`${BASE_URL}cart/set-count`, body)
+export const postCartSubmit = () => POST(`${BASE_URL}cart/submit`)
 
 //* DELETE
-export const deleteCartAll = () => axios.delete(`${BASE_URL}cart/all`, getToken())
-export const deleteCartItem = (id) => axios.delete(`${BASE_URL}cart/${id}`, getToken())
+export const deleteCartAll = () => DELETE(`${BASE_URL}cart/all`)
+export const deleteCartItem = (id) => DELETE(`${BASE_URL}cart/${id}`)
