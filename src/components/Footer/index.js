5456c057a6ff8f7478ef864e1541d4264507753c
Filip Gadzo <filip.gadzo@fastlane.net>
2022-03-12 14:52:50 +0100

    Fixing footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,14 @@
-import '../NavigationBar/navigationCSS.css'
+import './footer.css'
 
 export default function Index() {
 	return (
-		<footer className="footer shadow-inner">
-			<small className="footer-text">
-				&copy; Copyright 2022, RIT Cage Inventory
+		<footer className="shadow-inner py-6 justify-between fixed inset-x-0 bottom-0 flex">
+				<p className='ml-16 mt-4 font-bold text-md'>&copy; Copyright 2022, RIT Cage Inventory</p>
 				<img
 					src={require('../../assets/rit-logo-rochester-institute-of-technology.png')}
 					alt="Rochester Institute of Technology Logo"
-					className="footer-logo w-1/6"
+					className="w-1/6 mb-2"
 				/>
-			</small>
 		</footer>
 	)
 }
